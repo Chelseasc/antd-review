0a63c102c95f7ecf943cc28ec80134ee446a3ec3
cyin6 <cyin6@volvocars.com>
2020-11-02 11:52:21 +0800

    init

---
+++ src/.umi/core/routes.ts
@@ -0,0 +1,24 @@
+// @ts-nocheck
+import React from 'react';
+import { ApplyPluginsType } from '/Users/mac/Desktop/code/antd/node_modules/@umijs/runtime';
+import * as umiExports from './umiExports';
+import { plugin } from './plugin';
+
+export function getRoutes() {
+  const routes = [
+  {
+    "path": "/",
+    "component": require('/Users/mac/Desktop/code/antd/src/page/HelloWorld').default,
+    "exact": true
+  }
+];
+
+  // allow user to extend routes
+  plugin.applyPlugins({
+    key: 'patchRoutes',
+    type: ApplyPluginsType.event,
+    args: { routes },
+  });
+
+  return routes;
+}
